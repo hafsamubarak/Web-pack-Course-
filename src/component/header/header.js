dec4346cf177ddbb6fe29b0b1505822d4dd1e942
hafsa ahmed <sooahmed83@yahoo.com>
2021-11-24 19:34:26 +0200

    Web Pack Demo

---
+++ src/component/header/header.js
@@ -0,0 +1,14 @@
+import "./style.css";
+const element = document.createElement("header");
+
+element.innerHTML = "Hiiiiiii Hafsa";
+element.classList.add("info");
+document.body.appendChild(element);
+import photo from "../../assets/image.png";
+const img = document.createElement("img");
+img.src = photo;
+img.style.width = "50px";
+img.style.height = "50px";
+element.appendChild(img);
+
+// nooo
